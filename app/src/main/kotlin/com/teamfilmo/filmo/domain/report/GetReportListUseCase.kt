c0a01dc2c94599f04ac5307ac15ef665a553e780
Sunghyeon <tjdgustjdan@gmail.com>
2024-11-27 19:05:30 +0900

    fix: 예외 처리 개선 2 (try ~ catch 제거)
    - Result의 onSuccess와 onFailure을 통한 예외 처리

--- app/src/main/kotlin/com/teamfilmo/filmo/domain/report/GetReportListUseCase.kt
+++ app/src/main/kotlin/com/teamfilmo/filmo/domain/report/GetReportListUseCase.kt
@@ -21,33 +21,18 @@ class GetReportListUseCase
         operator fun invoke(lastReportId: String?): Flow<SearchReportResponse?> =
             flow {
                 // 1. 레포지토리 호출 시 예외 발생
-                try {
-                    val result = reportRepository.searchReport(SearchReportRequest(lastReportId = lastReportId))
-                    result
-                        .onSuccess { searchResult ->
-                            emit(searchResult)
-                        }.onFailure { exception ->
-                            when (exception) {
-                                // todo : 이후 UiState를 통해 HttpError 발생 시 에러 메시지 보이기
-                                is HttpException -> Timber.e("Network error: ${exception.message}")
-                                else -> Timber.e("Unknown error: ${exception.message}")
-                            }
-                            // 예외 발생 시  기본값  반환
-                            emit(SearchReportResponse())
+                val result = reportRepository.searchReport(SearchReportRequest(lastReportId = lastReportId))
+                result
+                    .onSuccess { searchResult ->
+                        emit(searchResult)
+                    }.onFailure { exception ->
+                        when (exception) {
+                            // todo : 이후 UiState를 통해 HttpError 발생 시 에러 메시지 보이기
+                            is HttpException -> Timber.e("Network error: ${exception.message}")
+                            else -> Timber.e("Unknown error: ${exception.message}")
                         }
-                } catch (e: Exception) {
-                    // result.onFailure에서 처리되지 않은 모든 예외를 최종적으로 잡아낸다.
-                    // 예상치 못한 예외 발생 가능성
-                    // 1. JSON 파싱 오류
-                    // 2. 라이브러리 내부 버그로 인한 예외
-                    Timber.e(e, "Unexpected error in GetReportListUseCase")
-                    // 예외 발생 시  기본값  반환
-                    emit(SearchReportResponse())
-                }
+                        // 예외 발생 시  기본값  반환
+                        emit(SearchReportResponse())
+                    }
             }
     }
-
-// operator fun invoke(targetId: String): Flow<Result<List<SearchReportItem>>> =
-//    reportRepository.searchUserReport(
-//        SearchUserReportListRequest(targetId),
-//    )
