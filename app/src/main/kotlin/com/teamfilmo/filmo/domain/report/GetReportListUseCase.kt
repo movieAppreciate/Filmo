b50a0941f744dab55e6b5390cc538893f5eb759f
Sunghyeon <tjdgustjdan@gmail.com>
2024-11-27 18:47:47 +0900

    comment: 예외 처리 개선
    - 주석 수정

--- app/src/main/kotlin/com/teamfilmo/filmo/domain/report/GetReportListUseCase.kt
+++ app/src/main/kotlin/com/teamfilmo/filmo/domain/report/GetReportListUseCase.kt
@@ -32,7 +32,7 @@ class GetReportListUseCase
                                 is HttpException -> Timber.e("Network error: ${exception.message}")
                                 else -> Timber.e("Unknown error: ${exception.message}")
                             }
-                            // 실패임을 확인할 수 잇도록 null 반환
+                            // 예외 발생 시  기본값  반환
                             emit(SearchReportResponse())
                         }
                 } catch (e: Exception) {
@@ -41,6 +41,7 @@ class GetReportListUseCase
                     // 1. JSON 파싱 오류
                     // 2. 라이브러리 내부 버그로 인한 예외
                     Timber.e(e, "Unexpected error in GetReportListUseCase")
+                    // 예외 발생 시  기본값  반환
                     emit(SearchReportResponse())
                 }
             }
